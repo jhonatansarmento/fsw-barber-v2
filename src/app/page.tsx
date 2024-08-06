f73d34722905b22b4d2ae2242702d25543e1ae14
Jhonatan Sarmento <jhonatan.sarmento@watch.tv.br>
2024-08-06 12:04:49 -0400

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import Header from "./_components/header"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
 
 export default function Home() {
@@ -30,6 +33,33 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
