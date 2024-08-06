96879f7fbaad31b5529edb53c42ef853a525172b
Jhonatan Sarmento <jhonatan.sarmento@watch.tv.br>
2024-08-06 11:44:42 -0400

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 export default function Home() {
   return (
-    <>
-      <h1>ola</h1>
-      <Button>Test</Button>
-    </>
+    <div>
+      <Header />
+      <div className="p-5">
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
