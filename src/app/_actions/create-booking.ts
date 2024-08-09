160324379c056ad6aff364416fde6b1a52f23015
Jhonatan Sarmento <jhonatan.sarmento@watch.tv.br>
2024-08-09 19:34:23 -0400

    feat: ensure barbershops can be searched by title and service

---
+++ src/app/_actions/create-booking.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { getServerSession } from "next-auth"
+import { revalidatePath } from "next/cache"
+import { authOptions } from "../_lib/auth"
+import { db } from "../_lib/prisma"
+
+interface CreateBookingParams {
+  serviceId: string
+  date: Date
+}
+
+export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário nao encontrado")
+  }
+  await db.booking.create({
+    data: { ...params, userId: (user.user as any).id },
+  })
+  revalidatePath("/barbershops/[id]")
+}
